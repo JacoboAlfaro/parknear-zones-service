cb234317dbc54866d1dc23c8534f7086c33abeb4
Stiven Carvajal <stivencarvjal205@gmail.com>
2026-05-13 00:28:33 -0500

    correccion lectura .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ZonasModule } from './zones/zones.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
@@ -9,16 +8,26 @@ import { RolesGuard } from './auth/guards/roles.guard';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ZonasModule,
 
-    JwtModule.register({
-      secret: 'secretKey',
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const expiresIn = '1h';
+
+        return {
+          secret: configService.get<string>('JWT_SECRET', 'secretKey'),
+          signOptions: {
+            expiresIn,
+          },
+        };
+      },
     }),
 
     PassportModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
     AppService,
     JwtStrategy,
